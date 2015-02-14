8a8a04c2911f81ef755019cbc4b85c8ad623ef72
Andrei I. Hava <hava.ionut@yahoo.com>
2015-02-14 18:37:32 +0200

    Updated to 0.0.2-SNAPSHOT.

--- src/main/java/com/katgamestudios/andyandreih/overseer/main/OverseerMain.java
+++ src/main/java/com/katgamestudios/andyandreih/overseer/main/OverseerMain.java
@@ -1,17 +1,25 @@
 package com.katgamestudios.andyandreih.overseer.main;
 
+import org.bukkit.plugin.InvalidPluginException;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class OverseerMain extends JavaPlugin {
     public static DatabaseController dbCtrl = new DatabaseController();
     public static CommandListener cmdExec = new CommandListener();
     public static EventListener eventListen = new EventListener();
 
+    public static String dataFolder;
+
     @Override
     public void onEnable() {
         dbCtrl.mainClass = this;
         cmdExec.mainClass = this;
         eventListen.mainClass = this;
+
+        dataFolder = getDataFolder().getAbsolutePath();
 
         if(!getDataFolder().exists()) {
             getDataFolder().mkdir();
@@ -24,11 +32,25 @@ public class OverseerMain extends JavaPlugin {
         getCommand("overseer").setExecutor(cmdExec);
         getLogger().info("Commands registered.");
 
-        dbCtrl.initDb(getDataFolder().getAbsolutePath());
+        dbCtrl.initDb(dataFolder);
         if(dbCtrl.openDb()) {
             dbCtrl.generateUsersTable();
             getLogger().info("Generated 'users' table.");
         }
         getLogger().info("Generated local database.");
+
+        File authFile = new File("OverseerAuth.jar");
+        Plugin authPlugin = null;
+        if(authFile.exists()) {
+            getLogger().info("Authentication module found. Attempting load...");
+            try {
+                authPlugin = getPluginLoader().loadPlugin(authFile);
+                getPluginLoader().enablePlugin(authPlugin);
+                getLogger().info("Authentication module loaded.");
+            } catch (InvalidPluginException e) {
+                e.printStackTrace();
+                getLogger().info("Failed to load authentication module.");
+            }
+        }
     }
 }
